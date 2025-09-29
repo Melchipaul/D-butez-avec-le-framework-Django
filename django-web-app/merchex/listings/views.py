30e778306f923a19a5e9f072fa4329fed52b1375
Melchicédec NDUWAYO <KN6611@engie.com>
2025-09-29 18:37:57 +0200

    Learning Django 29/09/2025

--- django-web-app/merchex/listings/views.py
+++ django-web-app/merchex/listings/views.py
@@ -37,6 +37,18 @@ def band_create(request):
         form = BandForm()
     return render(request, 'listings/band_create.html', {'form': form})
 
+def band_update(request, id):
+    band = Band.objects.get(id=id)
+
+    if request.method == 'POST':
+        form = BandForm(request.POST, instance=band)
+        if form.is_valid():
+            band = form.save()
+            return redirect('band-detail', band.id)
+    else:
+        form = BandForm(instance=band)
+    return render(request, 'listings/band_update.html', {'form': form})
+
 def about(request):
     return render(request, 'listings/about.html')
 
@@ -58,6 +70,18 @@ def create_new_listing(request):
     else:
         form = ListingForm()
     return render(request, 'listings/create_new_listing.html', {'form': form})
+
+def listing_update(request, id):
+    listing = Listing.objects.get(id=id)
+
+    if request.method == 'POST':
+        form = ListingForm(request.POST, instance=listing)
+        if form.is_valid():
+            listing = form.save()
+            return redirect('listing-detail', listing.id)
+    else:
+        form = ListingForm(instance=listing)
+    return render(request, 'listings/listing_update.html', {'form': form})
 
 def nous_contacter(request):
     if request.method == 'POST':
